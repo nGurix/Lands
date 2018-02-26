788964036cd82a684be9a5ad9556f20130b1db5a
eguidotti <enguro@gmail.com>
2018-02-26 18:32:50 -0300

    LoginViewModel

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemember { get; set; }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            IsRemember = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+        private void Login()
+        {
+
+        }
+        #endregion
+    }
+}
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,5 +6,15 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
