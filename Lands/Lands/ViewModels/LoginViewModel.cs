e01f21c9991dbb5551a1752ff89c4eaeea2204b2
eguidotti <enguro@gmail.com>
2018-02-26 23:08:07 -0300

    Singleton

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,24 +1,56 @@
 ﻿using GalaSoft.MvvmLight.Command;
+using Lands.Views;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Lands.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Email
+        {
+            get { return email; }
+            set { SetValue(ref email, value); }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { SetValue(ref password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set { SetValue(ref isRunning, value); }
+        }
         public bool IsRemember { get; set; }
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set { SetValue(ref isEnabled, value); }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             IsRemember = true;
+            IsEnabled = true;
         }
         #endregion
 
@@ -30,9 +62,37 @@ namespace Lands.ViewModels
                 return new RelayCommand(Login);
             }
         }
-        private void Login()
+        private async void Login()
         {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter an email", "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter an password", "Accept");
+                return;
+            }
+            IsRunning = true;
+            IsEnabled = false;
+            if (Email != "a@a.com" || Password != "123")
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert("Error", "Email or password incorrect", "Accept");
+                Password = string.Empty;
+                return;
+            }
+            IsRunning = false;
+            IsEnabled = true;
 
+            Email = string.Empty;
+            Password = string.Empty;
+
+            //se referecia el singleton, asi se asegura que la landviewmodel queda alinieada a la LAndsPage
+            MainViewModel.GetInstance().Lands = new LandsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new LandsPage());
         }
         #endregion
     }
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -8,12 +8,27 @@ namespace Lands.ViewModels
     {
         #region ViewModels
         public LoginViewModel Login { get; set; }
+        public LandsViewModel Lands { get; set; }
         #endregion
 
         #region Constructors
         public MainViewModel()
         {
+            instance = this;
             Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        //permite hacer un llamado de la mainviewmodel desde cualquier clase sin necesidad de instancear otra MainViewModel 
+        private static MainViewModel instance;
+        public static MainViewModel GetInstance()
+        {
+            if(instance == null)
+            {
+                return new MainViewModel(); 
+            }
+            return instance;
         }
         #endregion
     }
